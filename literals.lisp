525f67a1d71b6d710b5ac9228dd96e8469e31554
mikel evins <mevins@me.com>
2023-11-07 14:32:53 -0600

    removed dict literals; added hash-table literals

--- literals.lisp
+++ literals.lisp
@@ -15,16 +15,39 @@
 
 (set-macro-character #\] (get-macro-character #\)))
 
-;;; dicts
+;;; hash-tables
 ;;; ---------------------------------------------------------------------
-;;; enables us to write literal dicts like {:a 1 :b 2 :c {:d 4 :e 5}}
-;;; dicts created this way always use 'equal to test keys
+;;; enables us to write literal hash-tables like {:a 1 :b 2 :c {:d 4 :e 5}}
+;;; hash-tables created this way always use 'equal to test keys
 
 (set-syntax-from-char #\{ #\()
 (set-syntax-from-char #\} #\))
+
+(defmacro hash ((&key (test #'eql) (size 16)
+                      (rehash-size 2) (rehash-threshold 1))
+                &rest keys-vals-plist)
+  (let ((h (gensym "new-hash-table")))
+    `(let ((,h (make-hash-table :test ,test :size ,size
+                                :rehash-size ,rehash-size
+                                :rehash-threshold ,rehash-threshold)))
+       ,@(loop
+               :with tmp = nil
+               :for e :in keys-vals-plist
+               :for i :from 0
+               :if (evenp i) :do (setf tmp e)
+               :collect `(setf (gethash ,tmp ,h) ,e))
+       ,h)))
+
+(defmethod print-object ((object hash-table) stream)
+  (if (= (hash-table-count object) 0)
+      (format stream "hashtable {}")
+      (let ((data (loop for k being the hash-keys of object
+                        collect k
+                        collect (gethash k object))))
+        (format stream "{~{~s~^ ~}}" data))))
 
 (set-macro-character #\{
                      (lambda (stream char)
                        (declare (ignore char))
                        (let ((elts (read-delimited-list #\} stream t)))
-                         `(dict 'equal ,@elts))))
+                         `(hash (:test 'equal) ,@elts))))
